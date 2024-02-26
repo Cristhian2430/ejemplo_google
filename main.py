2fa0795091037dc70655fd3297a33896a90bcf5e
Cristhian2430 <82115251+Cristhian2430@users.noreply.github.com>
2024-02-25 23:57:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '¡Hola, mundo! Esta es una aplicación web desplegada en Cloud Run. Importado 60'
+    return '¡Hola, mundo! Esta es una aplicación web desplegada en Cloud Run. Importado 59/58'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8501)
